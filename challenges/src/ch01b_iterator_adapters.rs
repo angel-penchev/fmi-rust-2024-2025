422d720ad70d24c98cde98f56c356c0e419e0ac2
Angel Penchev <angel@penchev.dev>
2024-11-19 15:15:07 +0200

    feat(ch02): iterator adapters part 2
    Added a solution for part 2 of the iterator adapters challenge.

---
+++ challenges/src/ch01b_iterator_adapters.rs
@@ -0,0 +1,31 @@
+pub struct DynMapIter<T, U> {
+    iter: Box<dyn Iterator<Item = T>>,
+    func: Box<dyn Fn(T) -> U>,
+}
+
+impl<T, U> DynMapIter<T, U> {
+    pub fn new(iter: Box<dyn Iterator<Item = T>>, func: Box<dyn Fn(T) -> U>) -> DynMapIter<T, U> {
+        DynMapIter { iter, func }
+    }
+}
+
+impl<T, U> Iterator for DynMapIter<T, U> {
+    type Item = U;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next().map(|x| (self.func)(x))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_dyn_map_iter() {
+        let xs = &["x", "xx", "xxx"];
+        let map_iter = DynMapIter::new(Box::new(xs.iter()), Box::new(|s| s.len()));
+
+        assert_eq!(map_iter.collect::<Vec<_>>(), vec![1, 2, 3]);
+    }
+}
--- challenges/src/lib.rs
+++ challenges/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod ch01a_iterator_adapters;
+pub mod ch01b_iterator_adapters;
