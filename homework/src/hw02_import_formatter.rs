de9867716be4ec78f8768bcc41845a8bf50c2afd
Angel Penchev <angel@penchev.dev>
2024-12-02 22:37:05 +0200

    feat(hw02): homework 2 init + import display trait
    Initialized homework 2.
    Added Display trait for Import.
    Implemented format_flat original order case and test case for it.

---
+++ homework/src/hw02_import_formatter.rs
@@ -0,0 +1,48 @@
+use std::fmt;
+
+pub struct Import<'a>(pub &'a [&'a str]);
+
+pub enum Order {
+    Original,
+    Sorted,
+}
+
+// As per Clippy recommendation, implemented fmt::Display instead of ToString trait.
+impl<'a> fmt::Display for Import<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0.join("::"))
+    }
+}
+
+pub fn format_flat(imports: &[Import], order: Order) -> Vec<String> {
+    imports.iter().map(|i| i.to_string()).collect()
+}
+
+pub fn format_nested(imports: &[Import], order: Order) -> Vec<String> {
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    /// Validate that the `format_flat` function works as expected, if the original order is kept.
+    #[test]
+    fn test_format_flat_original() {
+        let imports = &[
+            Import(&["my_crate", "a"]),
+            Import(&["my_crate", "b", "B1"]),
+            Import(&["my_crate", "b", "B2"]),
+        ];
+        let order = Order::Original;
+
+        let expected_result = vec![
+            String::from("my_crate::a"),
+            String::from("my_crate::b::B1"),
+            String::from("my_crate::b::B2"),
+        ];
+        let actual_result = format_flat(imports, order);
+
+        assert_eq!(expected_result, actual_result);
+    }
+}
--- homework/src/lib.rs
+++ homework/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod hw01_fibonacci_words;
+pub mod hw02_import_formatter;
